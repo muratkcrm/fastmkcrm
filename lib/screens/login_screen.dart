c69f6dd29ed165345c5e7672fc22c278ac8c29d4
muratkcrm <49165382+muratkcrm@users.noreply.github.com>
2023-01-03 00:02:00 +0300

    fastmkcrm5

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,26 +12,8 @@ class LoginScreen extends StatelessWidget {
         height: double.infinity,
         child: Stack(
           children: [
-            Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Color.fromRGBO(63, 63, 156, 1),
-                Color.fromRGBO(90, 70, 156, 1),
-              ])),
-              width: double.infinity,
-              height: size.height * 0.4,
-            ),
-            SafeArea(
-              child: Container(
-                margin: const EdgeInsets.only(top: 30),
-                width: double.infinity,
-                child: const Icon(
-                  Icons.person_pin,
-                  color: Colors.white,
-                  size: 100,
-                ),
-              ),
-            ),
+            cajapurpura(size),
+            iconpersonal(),
             Column(
               children: [
                 const SizedBox(height: 250),
@@ -67,6 +49,60 @@ class LoginScreen extends StatelessWidget {
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Container cajapurpura(Size size) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromRGBO(63, 63, 156, 1),
+        Color.fromRGBO(90, 70, 178, 1),
+      ])),
+      width: double.infinity,
+      height: size.height * 0.4,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 90,
+            left: 30,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: const Color.fromRGBO(255, 255, 255, 0.05),
+              ),
+            ),
+          ),
+          Positioned(
+            top: -40,
+            left: -30,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: const Color.fromRGBO(255, 255, 255, 0.05),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SafeArea iconpersonal() {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.only(top: 30),
+        width: double.infinity,
+        child: const Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 100,
         ),
       ),
     );
